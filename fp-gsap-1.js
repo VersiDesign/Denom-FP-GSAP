4cbb27ed5e72879a8b64e0a1bd194b2f8ebf8f20
MattC <matthew@versi.design>
2024-03-05 15:15:10 +1100

    Update fp-gsap-1.js

--- fp-gsap-1.js
+++ fp-gsap-1.js
@@ -203,7 +203,7 @@ document.addEventListener("DOMContentLoaded", function() {
             end: "top top",
             scrub: 1
         },
-        delay: 3 // Delay the start of the timeline
+        delay: 4 // Delay the start of the timeline
     });
 
     // Fade in circles together
@@ -212,7 +212,7 @@ document.addEventListener("DOMContentLoaded", function() {
       .to(".fp-gap__circle-wrap--left", { x: "-=70vw", duration: 6, ease: "power3.inOut" }, "<")
       .to(".fp-gap__circle-wrap--right", { x: "+=70vw", duration: 6, ease: "power3.inOut" }, "<")
       // Fade in text inside circles earlier
-      .to(".fp-circle-txt", { opacity: 1, duration: 1, ease: "power1.inOut" }, "<0.5")
+      .to(".fp-circle-txt", { opacity: 1, duration: 1.5, ease: "power1.inOut" }, "1")
       // Fade in '.fp-gap__txt-wrap' when the circles are midway through their movement
       .to(".fp-gap__txt-wrap", { opacity: 1, duration: 2, ease: "power1.inOut" }, "2");
 }
