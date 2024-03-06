6a6dea6ebdfe99283d1ab90e7291372e5b769394
MattC <matthew@versi.design>
2024-03-06 19:46:57 +1100

    Update fp-gsap-1.js

--- fp-gsap-1.js
+++ fp-gsap-1.js
@@ -318,7 +318,7 @@ function animateBubble(bubble) {
     gsap.set(bubble, { x: `+=${window.innerWidth}px` });
 
     // Define the duration for the bubble to travel across the screen
-    const travelDuration = gsap.utils.random(15, 25); // Randomize duration for more natural movement
+    const travelDuration = gsap.utils.random(15, 35); // Randomize duration for more natural movement
 
     // Horizontal movement
     const moveHorizontally = () => {
@@ -333,8 +333,8 @@ function animateBubble(bubble) {
 
     // Vertical movement with random Y position
     gsap.to(bubble, {
-        y: `+=${gsap.utils.random(-100, 100)}`, 
-        duration: () => gsap.utils.random(5, 10), 
+        y: `+=${gsap.utils.random(-200, 200)}`, 
+        duration: () => gsap.utils.random(5, 20), 
         ease: "sine.inOut",
         repeat: -1, 
         yoyo: true
