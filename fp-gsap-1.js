1b082bf9024bd316aabab7b376c3643b0c5f8418
MattC <matthew@versi.design>
2024-03-07 12:50:28 +1100

    Update fp-gsap-1.js

--- fp-gsap-1.js
+++ fp-gsap-1.js
@@ -376,7 +376,7 @@ function statCounterAnimation({ counterSelector, triggerSelector, includePlus, a
 
     ScrollTrigger.create({
         trigger: triggerSelector,
-        start: 'top bottom',
+        start: 'top 90%',
         end: "bottom top",
         onEnter: () => {
             gsap.to({}, {
