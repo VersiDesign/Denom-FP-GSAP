9805e157654df75f8ba93e113dde5ddc7f57db0e
MattC <matthew@versi.design>
2024-03-10 17:27:47 +1100

    Update fp-gsap-3.js

--- fp-gsap-3.js
+++ fp-gsap-3.js
@@ -361,7 +361,7 @@ function animateAllBubbles() {
     const bubbles = document.querySelectorAll('.fp-bubbles__circle-wrap--bbl-1, .fp-bubbles__circle-wrap--bbl-2, .fp-bubbles__circle-wrap--bbl-3, .fp-bubbles__circle-wrap--bbl-4, .fp-bubbles__circle-wrap--bbl-5, .fp-bubbles__circle-wrap--bbl-6');
     bubbles.forEach(bubble => {
         // Generate a random start delay for each bubble, e.g., between 0 and x seconds
-        const startDelay = gsap.utils.random(0, 10, false);
+        const startDelay = gsap.utils.random(0, 12, false);
         animateBubble(bubble, startDelay);
     });
 }
